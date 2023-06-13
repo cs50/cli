bf32525d7679a2f15a3d82702254b730f9a20e79
David J. Malan <malan@harvard.edu>
2023-06-12 22:41:33 -0400

    removed MANSECT as no longer needed, also was disabling section 5

--- etc/profile.d/cli.sh
+++ etc/profile.d/cli.sh
@@ -46,9 +46,6 @@ if [ "$(whoami)" != "root" ]; then
     export CFLAGS="-ferror-limit=1 -gdwarf-4 -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-gnu-folding-constant -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-variable -Wshadow"
     export LDLIBS="-lcrypt -lcs50 -lm"
 
-    # Manual sections to search
-    export MANSECT=3,2,1
-
     # Node.js
     export NODE_ENV="dev"
 
